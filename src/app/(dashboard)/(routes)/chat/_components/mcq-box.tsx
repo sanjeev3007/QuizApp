5c795b1d8669ae7d0b5784e1e0163cbc72fe0fb6
Aaryan6 <aaryanpatel683@gmail.com>
2024-02-12 21:16:59 +0530

    add explaiantion ai to question box

--- src/app/(dashboard)/(routes)/chat/_components/mcq-box.tsx
+++ src/app/(dashboard)/(routes)/chat/_components/mcq-box.tsx
@@ -3,7 +3,7 @@
 import { Bot } from "lucide-react";
 import OptionsBox from "./options-box";
 import QuestionBox from "./question-box";
-
+import ExplainationPopover from "./explaination-popover";
 export default function MCQBox({
   currentQuestion,
   handleNext,
@@ -15,12 +15,15 @@ export default function MCQBox({
   submissions: any;
   questionIndex: number;
 }) {
+  const correctAnswer = JSON.parse(currentQuestion?.options).find(
+    (option: any) => option.correct == "true"
+  )?.text;
   return (
     <div className="max-w-lg my-2 flex items-start w-full gap-x-2">
       <div className="bg-orange-300 w-10 h-10 rounded-full grid place-items-center">
         <Bot size={20} className="stroke-white" />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 relative">
         <QuestionBox
           question={currentQuestion?.question}
           questionIndex={questionIndex}
@@ -30,6 +33,11 @@ export default function MCQBox({
           handleNext={handleNext}
           submissions={submissions}
           questionId={currentQuestion?.uuid}
+        />
+
+        <ExplainationPopover
+          question={currentQuestion?.question}
+          answer={correctAnswer}
         />
       </div>
     </div>
---
+++ src/app/api/explain-bot/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+import { StreamingTextResponse, OpenAIStream } from "ai";
+
+const perplexity = new OpenAI({
+  apiKey: process.env.TOGETHER_AI_API_KEY || "",
+  baseURL: "https://api.together.xyz",
+});
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { prompt: question, correctOption } = body;
+
+  const response = await perplexity.chat.completions.create({
+    model: "NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
+    stream: true,
+    temperature: 0,
+    messages: [
+      {
+        role: "user",
+        content: `Given the question and its right answer, Your work is to explain me it in easy language and in the minimum words. This is the question: ${question}\nand the correct option is: ${correctOption}\n explain it to me.`,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream, {
+    status: 200,
+  });
+}
