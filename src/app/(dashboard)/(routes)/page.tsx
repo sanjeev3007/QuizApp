682ad39b4706042e57215fccdfdd2a8b6599ab61
Aaryan6 <aaryanpatel683@gmail.com>
2024-02-14 14:48:51 +0530

    Remove unused imports and commented code

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,20 +1,10 @@
-import { getQuestions, getSession } from "@/app/supabase-server";
 import DetailsDialog from "@/components/details-dialog";
 import HistoryCard from "@/components/history-card";
 import InitialAssessmentCard from "@/components/initial-assessment-card";
 import InitialAssessmentDialog from "@/components/initial-assessment-dialog";
-import MCQ from "@/components/mcq";
 import QuizMeCard from "@/components/quiz";
-import { redirect } from "next/navigation";
 
 const Home = async () => {
-  // const session = await getSession();
-  // const user = session?.user;
-  // // const isAssessmentDone = await userAssessmentStatus(user?.id!);
-  // if (!user) {
-  //   return redirect("/initial-assessment");
-  // }
-
   return (
     <div className="p-12 max-w-6xl mx-auto">
       <div className="flex items-center">
@@ -29,7 +19,6 @@ const Home = async () => {
       </div>
       <div className="grid gap-4 mt-4 md:grid-cols-1">
         <QuizMeCard />
-        {/* <HistoryCard /> */}
       </div>
       <InitialAssessmentDialog />
     </div>
