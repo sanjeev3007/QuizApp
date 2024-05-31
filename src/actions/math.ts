7b378fd36debe8fe2aab741df61f4faaae694b41
Aaryan6 <aaryanpatel683@gmail.com>
2024-05-31 15:19:00 +0530

    using user grade by default for storing

--- src/actions/math.ts
+++ src/actions/math.ts
@@ -39,7 +39,7 @@ export async function updateMathQuiz(
 
   if (assignedData) {
     metadata = {
-      grade: assignedData?.currentGrade,
+      grade: grade,
       topic: topic,
       assignedGrade: assignedData?.topic?.grade,
     };
