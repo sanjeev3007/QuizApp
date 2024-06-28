cdddb7227b1781d19865f6271a9a3978ebf7cb6e
raghav-codeyoung <raghav@smaowl.com>
2024-06-28 16:50:01 +0530

    initital format

---
+++ src/app/(dashboard)/(routes)/student-dashboard/page.tsx
@@ -0,0 +1,15 @@
+const PageContent = async () => {
+  return (
+
+  );
+};
+
+const Page = () => {
+  return (
+    <div className="p-5 md:px-12 w-full md:max-w-7xl mx-auto bg-[#FFF] !important">
+        <PageContent />
+    </div>
+  );
+};
+
+export default Page;
