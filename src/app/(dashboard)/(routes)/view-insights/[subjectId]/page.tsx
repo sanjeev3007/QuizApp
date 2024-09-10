afd303ca074815eaf390316922c3a587df7ba079
Aaryan6 <aaryanpatel683@gmail.com>
2024-09-10 13:49:57 +0530

    replace the insights apis to insights functions

--- src/app/(dashboard)/(routes)/view-insights/[subjectId]/page.tsx
+++ src/app/(dashboard)/(routes)/view-insights/[subjectId]/page.tsx
@@ -3,7 +3,7 @@ import React, { Suspense } from "react";
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
 import CircularProgress from "@mui/material/CircularProgress";
-import { getDashboardData, getInsightsData } from "../_utils";
+import { getDashboard, getInsight } from "@/app/supabase-server";
 
 type Props = {
   subjectId: number;
@@ -13,8 +13,8 @@ const PageContent = async (props: Props) => {
   const user_Id = getCookie("userId", { cookies });
   const grade = getCookie("grade", { cookies });
   const [dashboardData, insightData] = await Promise.all([
-    getDashboardData(user_Id!, props.subjectId),
-    getInsightsData(user_Id!, props.subjectId, parseInt(grade!)),
+    getDashboard(user_Id!, props.subjectId),
+    getInsight(user_Id!, props.subjectId, parseInt(grade!)),
   ]);
   return (
     <div className="p-5 md:p-12 w-full md:max-w-5xl mx-auto bg-[#FFF] !important">
