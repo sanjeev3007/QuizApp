10c157e168d61df90c7e8ccb7bd9293f9721af8f
Aaryan6 <aaryanpatel683@gmail.com>
2024-10-01 15:28:43 +0530

    add suspense

--- src/app/(dashboard)/(routes)/languages/page.tsx
+++ src/app/(dashboard)/(routes)/languages/page.tsx
@@ -1,7 +1,14 @@
 import { getLanguageLevels } from "@/actions/language.actions";
 import LanguageDashboard from "./components/language-dashboard";
+import { Suspense } from "react";
 
 export default async function DashboardPage() {
   const levels = await getLanguageLevels();
-  return <LanguageDashboard levels={levels!} />;
+  return (
+    <div>
+      <Suspense>
+        <LanguageDashboard levels={levels!} />
+      </Suspense>
+    </div>
+  );
 }
